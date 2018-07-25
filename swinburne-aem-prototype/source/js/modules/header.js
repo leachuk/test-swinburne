a39fa970d1e57b484ebcd5e80b92d9811d911053
stephane_swinburne <stephane.lemaitre@isobar.com>
2018-07-25 10:42:30 +1000

    [AMIS3-357] initial commit

--- swinburne-aem-prototype/source/js/modules/header.js
+++ swinburne-aem-prototype/source/js/modules/header.js
@@ -1,40 +1,16 @@
-import Sticky from './header/sticky'
+
 
 export default () => {
-  // Bind a click handler to the navigation bar when viewing it on a mobile device, this
-  // allows a user to hide it when they click on the blacked out area.
-  $('.navbar-collapse').off('click.navbar.toggle').on('click.navbar.toggle', e => {
-    const $target = $(e.target)
+  $(document).ready( () => {
+    let $img_paralax = $('.logo-wrapper > div:last-of-type > img');
+    let $window = $(window);
+    let $window_width = $window.width();
 
-    // Did the user click on the empty space around the nav?
-    if ($target.is('.navbar-collapse') && $(window).width() < 992) {
-      $target.collapse('hide')
-
-      // Restore the page scrollbar
-      const scrollOffset = parseInt($('body').css('top'), 10)
-
-      $('body').removeClass('no-scroll').css('top', 'auto')
-      $(window).scrollTop(-scrollOffset)
-    }
-  })
-
-  // Bind a click handler to the toggler
-  $('.navbar-toggler').off('click.navbar.toggler').on('click.navbar.toggler', () => {
-    const scrollOffset = $(window).scrollTop()
-
-    // Prevent the scroll on the body of the page and set the top offset to the current scroll
-    // position so the user remains at the same offset.
-    $('body').addClass('no-scroll').css('top', -scrollOffset)
-  })
-
-  // Bind a click handler to the entire page that detects when the user clicks outside the search
-  // box on mobile which closes it when open.
-  $(document.body).off('click.header.globalSearch').on('click.header.globalSearch', e => {
-    if (!$(e.target).parents('[data-modules*="global-search"]').length) {
-      $(document.body).removeClass('global-search')
-    }
-  })
-
-  // Sticky behaviour
-  Sticky()
+    $window.scroll( () => {
+      if($window_width > 991) {
+        let scrollTop = 0 > $window.scrollTop() ? 0 : -1 * $window.scrollTop();
+        $img_paralax.css('margin-top', scrollTop);
+      }
+    });
+  });
 }

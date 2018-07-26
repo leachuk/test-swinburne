7697ff66ac569eb01df62e6c13d70f7c8d42ba2d
stephane_swinburne <stephane.lemaitre@isobar.com>
2018-07-26 11:06:02 +1000

    [AMIS3-357] Added header module.

--- swinburne-aem-prototype/source/js/modules/header.js
+++ swinburne-aem-prototype/source/js/modules/header.js
@@ -2,12 +2,12 @@
 
 export default () => {
   $(document).ready( () => {
-    let $img_paralax = $('.logo-wrapper > div:last-of-type > img');
+    let $img_paralax = $('.logo-wrapper > div[component]:last-of-type  img');
     let $window = $(window);
-    let $window_width = $window.width();
+    let window_width = $window.width();
 
     $window.scroll( () => {
-      if($window_width > 991) {
+      if(window_width > 991) {
         let scrollTop = 0 > $window.scrollTop() ? 0 : -1 * $window.scrollTop();
         $img_paralax.css('margin-top', scrollTop);
       }

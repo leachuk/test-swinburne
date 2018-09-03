5aa6338c4f3b69129794c813b5a3a18601bbd421
stephane_swinburne <stephane.lemaitre@isobar.com>
2018-09-03 12:56:43 +1000

    [S3KMD-788] Fixed navigation and on mobile.

--- swinburne-aem-prototype/source/js/modules/navtoggler.js
+++ swinburne-aem-prototype/source/js/modules/navtoggler.js
@@ -4,9 +4,10 @@ export default () => {
   $(document).ready( () => {
       let $navToggler = $('[data-modules="navtoggler"]');
       let $navBar = $('div[component].navbar-nav ul');
+      let $body = $('body');
 
       $navToggler.click(function(){
-        $(this).toggleClass('collapsed');
+        $body.toggleClass('collapsed');
         $navBar.slideToggle();
       });
   });

590cfe2aecaea17ab01d5bfaa36993a9dca00ea4
stephane_swinburne <stephane.lemaire@isobar.com>
2018-10-18 13:54:51 +1100

    [S3KMD-1039] Adding Loading overlay on autocomplete inputs.

--- swinburne-aem-content/src/main/content/jcr_root/etc/clientlibs/swinburne/forms/themes/jieTheme/js/theme.js
+++ swinburne-aem-content/src/main/content/jcr_root/etc/clientlibs/swinburne/forms/themes/jieTheme/js/theme.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
     $(".describe-you select").selectmenu();
+    var $formContainer = $("body");
+
+	//Catch Ajax requests for hiding and and showing overlay
+    $(this).ajaxStart(function (ev) {
+        console.log(ev);
+        $formContainer.addClass('loading');
+    });
+    
+    $(this).ajaxComplete(function () {
+        $formContainer.removeClass('loading');
+    });
+
 });
+
+//function to validate auto complete fields.
+function validateSearch($field, items) {
+  var val = $field.val();
+  var hasMatch = false;
+  items.values.map(function (item) {                      
+    if(item.value.toLowerCase().indexOf(val.toLowerCase()) !== -1) {
+      hasMatch = true;
+    }
+  });
+  val = !hasMatch ? "" : val;
+  $field.val(val);
+}
+
+function isValuePresent(val, items) {
+  var hasMatch = false;
+  items.values.map(function (item) {                      
+    if( item.value.toLowerCase() === val.toLowerCase() ) {
+      hasMatch = true;
+    }
+  });
+  return hasMatch;
+}

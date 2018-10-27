2a6a35588bf7f6a09cb5e315ae65788eed35e0fe
stephane_swinburne <stephane.lemaire@isobar.com>
2018-10-28 10:19:35 +1100

    [S3KMD-1071] Close mobile nav menu when nav is open.

--- swinburne-aem-prototype/source/js/modules/navtoggler.js
+++ swinburne-aem-prototype/source/js/modules/navtoggler.js
@@ -4,10 +4,16 @@ export default () => {
   $(document).ready( () => {
       let $navToggler = $('[data-modules="navtoggler"]');
       let $navBar = $('div[component].navbar-nav ul');
+      let $menuItems = $('div[component].navbar-nav ul > li');
 
-      $navToggler.click(function(){
-        $(this).toggleClass('collapsed');
+      $navToggler.click((e) => {
+        $(e.target).toggleClass('collapsed');
         $navBar.slideToggle();
+      });
+
+      $menuItems.click(() => {
+          $navToggler.removeClass('collapsed');
+          $navBar.slideUp();
       });
   });
 }

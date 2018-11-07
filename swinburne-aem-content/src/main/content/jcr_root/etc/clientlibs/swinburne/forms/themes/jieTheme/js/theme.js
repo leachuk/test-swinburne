d373cfe012794155a6d83b4424cec2c784f9d656
stephane_swinburne <stephane.lemaire@isobar.com>
2018-11-07 12:20:36 +1100

    [S3KMD-1069] Close drops downs on focus out.

--- swinburne-aem-content/src/main/content/jcr_root/etc/clientlibs/swinburne/forms/themes/jieTheme/js/theme.js
+++ swinburne-aem-content/src/main/content/jcr_root/etc/clientlibs/swinburne/forms/themes/jieTheme/js/theme.js
@@ -17,6 +17,11 @@ $(document).ready(function() {
     //Init custom drop downs
     initDropDowns($selects);
 
+    // Close dropdown on blur
+    $('.ui-selectmenu-button').blur( function(){
+        $(this).prev().selectmenu( "close" );
+    });
+
     //Tooltips hover handler when screen is not a touchscreen
     var $tooltips = $('.guideHelpQuestionMark');
     if (!("ontouchstart" in document.documentElement)) {

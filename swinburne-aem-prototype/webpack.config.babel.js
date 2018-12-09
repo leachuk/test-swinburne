03f7c70256e6e6a4741a81e4e2d48bcdfecd9902
Rohan Milton <rohanmilton@Rohans-MacBook-Pro.local>
2018-12-10 09:00:53 +1100

    [SWU-396] Added template for Styleguide

--- swinburne-aem-prototype/webpack.config.babel.js
+++ swinburne-aem-prototype/webpack.config.babel.js
@@ -10,11 +10,14 @@ const config                      = require('./config.json')
 // const patternEngines              = require('patternlab-node/core/lib/pattern_engines')
 const merge                       = require('webpack-merge')
 const customization               = require('./source/_app/webpack.app.js');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const exec = require('child_process').exec;
 
 module.exports = (env) => {
   const { ifProd, ifDev } = getIfUtils(env)
-
+  console.log('env', env);
   if (!env.version) {
     console.log('Specify a version when running webpack eg --env.version="microsites"');
     return;
@@ -36,7 +39,7 @@ module.exports = (env) => {
 
     output: {
       filename : '[name].js',
-      path     : resolve(__dirname, 'public'),
+      path     : resolve(config.paths.public[env.version].root)
     },
 
     plugins: removeEmpty([
@@ -51,50 +54,52 @@ module.exports = (env) => {
           // Copy all images from source to public
           context : resolve(config.paths.source[env.version].images),
           from    : './**/*.*',
-          to      : resolve(config.paths.public.images),
+          to      : resolve(config.paths.public[env.version].images),
         },
         {
           // Copy favicon from source to public
           context : resolve(config.paths.source[env.version].root),
           from    : './*.ico',
-          to      : resolve(config.paths.public.root),
+          to      : resolve(config.paths.public[env.version].root),
         },
         {
           // Copy all web fonts from source to public
           context : resolve(config.paths.source[env.version].fonts),
           from    : './**/*.*',
-          to      : resolve(config.paths.public.fonts),
+          to      : resolve(config.paths.public[env.version].fonts),
         },
         {
           // Copy all css from source to public
           context : resolve(config.paths.source[env.version].css),
           from    : './*.css',
-          to      : resolve(config.paths.public.css),
+          to      : resolve(config.paths.public[env.version].css),
         },
         {
           // Styleguide Copy everything but css
           context : resolve(config.paths.source[env.version].styleguide),
           from    : './**/*',
-          to      : resolve(config.paths.public.root),
+          to      : resolve(config.paths.public[env.version].root),
           ignore  : ['*.css'],
         },
         {
           // Styleguide Copy and flatten css
           context : resolve(config.paths.source[env.version].styleguide),
           from    : './**/*.css',
-          to      : resolve(config.paths.public.styleguide, 'css'),
+          to      : resolve(config.paths.public[env.version].styleguide, 'css'),
           flatten : true,
         },
       ]),
       new EventHooksPlugin({
         ['done'](compilation, callback) {
-          const child = exec('npm run aem:frontend');
-          child.stdout.on('data', function (data) {
-            process.stdout.write(data);
-          });
-          child.stderr.on('data', function (data) {
-            process.stderr.write(data);
-          });
+          if (env.deploy) {
+            const child = exec(`./deploy-front-end ${env.version}`);
+            child.stdout.on('data', function (data) {
+              process.stdout.write(data);
+            });
+            child.stderr.on('data', function (data) {
+              process.stderr.write(data);
+            });
+          }
         }
       }),
     ]),
@@ -102,7 +107,7 @@ module.exports = (env) => {
     devServer: {
       host             : "0.0.0.0",
       useLocalIp       : true,
-      contentBase      : resolve(__dirname, 'public'),
+      contentBase      : resolve(config.paths.source[env.version].root),
       port             : config.server.port,
       open             : true,
       watchContentBase : false,

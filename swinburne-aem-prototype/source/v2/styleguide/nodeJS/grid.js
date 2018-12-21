2da426f9c59b59cdaab84e3ede257b9b60dec44e
Rohan Milton <rohanmilton@Rohans-MacBook-Pro.local>
2018-12-21 11:41:24 +1100

    [SWU-398] Added tests for grid and typography

--- swinburne-aem-prototype/source/v2/styleguide/nodeJS/grid.js
+++ swinburne-aem-prototype/source/v2/styleguide/nodeJS/grid.js
@@ -1,16 +1,17 @@
 let fs = require('fs');
 
-let template = `<div class="row">`;
 const breakpoints = ["" , "-sm", "-md", "-lg", "-xl"];
 const grids = [
-  {value :3, repeat: 4, bg: "bg-c1"},
-  {value :4, repeat: 3, bg: "bg-c2"},
-  {value :6, repeat: 2, bg: "bg-c4"},
-  {value :2, repeat: 6, bg: "bg-c5"},
-  {value :1, repeat: 12, bg: "bg-c6"}
+  {value :3, repeat: 4, bg: "bg-c1", id: "4-columns-grid"},
+  {value :4, repeat: 3, bg: "bg-c2", id: "3-columns-grid"},
+  {value :6, repeat: 2, bg: "bg-c4", id: "2-columns-grid"},
+  {value :2, repeat: 6, bg: "bg-c5", id: "6-columns-grid"},
+  {value :1, repeat: 12, bg: "bg-c6", id: "12-columns-grid"}
 ];
 
+let template = '';
 grids.map((grid) => {
+  template += `<div class="row mb-40" id=${grid.id}>`;
   template += `<div class="col-12"><h4>${grid.repeat} columns grid</h4></div>`;
   breakpoints.map((breakpoint) => {
     let columns = new Array(grid.repeat);
@@ -18,10 +19,8 @@ grids.map((grid) => {
       template += `<div class="col${breakpoint}-${grid.value}"><div class="wrapper ${grid.bg}" style="min-height:60px;"><span>col${breakpoint}-${grid.value}</span></div></div>`;
     });
   });
+  template += `</div>`;
 });
-
-template += `</div>`;
-
 
 fs.writeFile ("../html/grid.html", template, function(err) {
     if (err) throw err;
---
+++ swinburne-testing/src/test/groovy/specs/styleguide/guidelines/grid/GridScreenshotSpec.groovy
@@ -0,0 +1,35 @@
+package specs.styleguide.guidelines.grid
+
+import spock.lang.Stepwise
+import support.ComponentSpec
+
+@Stepwise
+class GridScreenshotSpec extends ComponentSpec {
+
+    String pathPage = "Styleguide-SWU/guidelines/grid"
+    String pathSite = "content/swinburne-showcase"
+    String language = "en"
+    String componentPath = "jcr:content/article/par/contentblock1/par/embed-source-grid"
+
+    def setupSpec() {
+        loginAsAdmin()
+    }
+
+    def "Appearance of 4 column grid"() {
+
+        given: '>I am in the component showcase page'
+        and: '>the component is on the showcase page'
+        def selector = "#embed-source-grid"
+
+        when: 'I am in the component showcase page'
+        setWindowSize()
+        waitForAuthorPreviewPage()
+
+        then: 'The component should appear on the page'
+        def component = waitForComponent(selector)
+
+        then: 'It should match reference image.'
+        designRef(selector)
+
+    }
+}

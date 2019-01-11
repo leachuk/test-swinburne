c027b1d5ad25b66f05c8fa0fbe998c03144b6001
Rohan Milton <rohanmilton@Rohans-MacBook-Pro.local>
2019-01-11 15:00:11 +1100

    [SWU-559] Created second yaml file for config

--- swinburne-aem-prototype/content-generator/generator.js
+++ swinburne-aem-prototype/content-generator/generator.js
@@ -13,6 +13,7 @@ const {
 const rootPath = 'content'
 const pathPrefixTags = 'content/cq:tags'
 const pathPrefixApps = 'apps/'
+const configPath = 'config'
 
 const {
   currentPath,
@@ -20,97 +21,146 @@ const {
   getBreakpointInfix,
   loadTemplateForCategory,
   parseTitle,
+  getDirectories,
+  isDirectory
 } = require('./functions')
 
 console.clear()
 
 try {
-  const configFilePath = currentPath('config.yml')
-  const config         = yaml.safeLoad(fs.readFileSync(configFilePath, 'utf8'))
 
-  rimraf(currentPath(rootPath), () => {
-    mkdirp.sync(currentPath(rootPath))
+  const parseFile = (configFilePath) => {
+    console.log('222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222', configFilePath);
+    if (!fs.existsSync(configFilePath)){
+      return;
+    }
+    console.log('3333');
+    const config         = yaml.safeLoad(fs.readFileSync(configFilePath, 'utf8'))
 
-    const categories = {}
-    for (const category of Object.keys(config)) {
-      const children = config[category]
+    rimraf(currentPath(rootPath), () => {
+      mkdirp.sync(currentPath(rootPath))
 
-      if (!children) {
-        continue
-      }
+      const categories = {}
+      for (const category of Object.keys(config)) {
+        const children = config[category]
 
-      // Create the object for the category
-      categories[category] = categories[category] || {}
-
-      for (const subcategory of Object.keys(children)) {
-        const data = children[subcategory]
-        let prefixes = data.prefixes
-
-        if (prefixes === undefined && data.prefix !== undefined) {
-          prefixes = [data.prefix]
+        if (!children) {
+          continue
         }
 
-        const contents    = data.contents
-        const prefixValue = data.prefixValue || ''
+        // Create the object for the category
+        categories[category] = categories[category] || {}
 
-        // Create the object for the sub-category
-        categories[category][subcategory] = categories[category][subcategory] || {
-          content: [],
-        }
+        for (const subcategory of Object.keys(children)) {
+          const data = children[subcategory]
+          let prefixes = data.prefixes
 
-        if (contents !== undefined) {
-          contents.forEach(content => {
-            categories[category][subcategory].content.push({
-              content  : content,
-              template : data.template || '',
-              type     : 'content',
+          if (prefixes === undefined && data.prefix !== undefined) {
+            prefixes = [data.prefix]
+          }
+
+          const contents    = data.contents
+          const prefixValue = data.prefixValue || ''
+
+          // Create the object for the sub-category
+          categories[category][subcategory] = categories[category][subcategory] || {
+            content: [],
+          }
+
+          if (contents !== undefined) {
+            contents.forEach(content => {
+              categories[category][subcategory].content.push({
+                content  : content,
+                template : data.template || '',
+                type     : 'content',
+              })
             })
-          })
-        }
+          }
 
-        if (prefixes !== undefined) {
-          for (const index in prefixes) {
-            if (!prefixes.hasOwnProperty(index)) {
-              return
-            }
+          if (prefixes !== undefined) {
+            for (const index in prefixes) {
+              if (!prefixes.hasOwnProperty(index)) {
+                return
+              }
 
-            let prefix           = prefixes[index]
-            let prefixIsAdvanced = false
-            let prefixTitle      = prefix
-            let prefixConfig     = {}
+              let prefix           = prefixes[index]
+              let prefixIsAdvanced = false
+              let prefixTitle      = prefix
+              let prefixConfig     = {}
 
-            // Is the prefix simple?
-            if (typeof prefix === 'object') {
-              prefixIsAdvanced = true
-              //simple name
-              prefix = Object.keys(prefix)[0]
-              //get prefix value as config
-              prefixConfig = prefixes[index][prefix]
-              //get title
-              prefixTitle = prefixConfig['title'] ? prefixConfig['title'] : prefix
-            }
+              // Is the prefix simple?
+              if (typeof prefix === 'object') {
+                prefixIsAdvanced = true
+                //simple name
+                prefix = Object.keys(prefix)[0]
+                //get prefix value as config
+                prefixConfig = prefixes[index][prefix]
+                //get title
+                prefixTitle = prefixConfig['title'] ? prefixConfig['title'] : prefix
+              }
 
-            let emptyTitle      = data.emptyTitle || false
-            let prefixToFolders = data.prefixToFolders || {}
-            let value           = prefix
-            let valueFormat     = data.valueFormat || ''
+              let emptyTitle      = data.emptyTitle || false
+              let prefixToFolders = data.prefixToFolders || {}
+              let value           = prefix
+              let valueFormat     = data.valueFormat || ''
 
-            if (data.startWith) {
-              value = data.startWith + value
-            }
+              if (data.startWith) {
+                value = data.startWith + value
+              }
 
-            if (isArray(data.breakpoints)) {
-              for (const bp of data.breakpoints) {
-                const infix = getBreakpointInfix(bp)
+              if (isArray(data.breakpoints)) {
+                for (const bp of data.breakpoints) {
+                  const infix = getBreakpointInfix(bp)
 
+                  // Does the data tree have sizes?
+                  if (isArray(data.sizes)) {
+                    for (const size of data.sizes) {
+                      const skipSize = (data.skipSizes || []).indexOf(size) !== -1
+
+                      let valueFormatted = valueFormat
+                        .replace(new RegExp('%%prefix%%', 'g'), prefix)
+                        .replace(new RegExp('%%infix%%', 'g'), infix ? `-${infix}` : '')
+
+                      if (!skipSize) {
+                        valueFormatted = valueFormatted.replace(new RegExp('%%size%%', 'g'), size)
+                      }
+
+                      categories[category][subcategory].content.push({
+                        prefixToFolders,
+                        flat           : data.flat || false,
+                        prefixValue    : prefixValue,
+                        template       : data.template || '',
+                        title          : parseTitle(data.title, prefix.toString(), { bp, emptyTitle, size }),
+                        type           : 'tag',
+                        value          : value + (infix ? `-${infix}` : '') + (!skipSize ? `-${size}` : ''),
+                        valueFormatted : valueFormatted
+                      })
+                    }
+
+                    // Nup, no sizing here
+                  } else {
+                    const valueFormatted = valueFormat
+                      .replace(new RegExp('%%prefix%%', 'g'), prefix)
+                      .replace(new RegExp('%%infix%%', 'g'), infix ? `-${infix}` : '')
+
+                    categories[category][subcategory].content.push({
+                      prefixToFolders,
+                      flat           : data.flat || false,
+                      prefixValue    : prefixValue,
+                      template       : data.template || '',
+                      title          : parseTitle(data.title, prefix.toString(), { bp, emptyTitle }),
+                      type           : 'tag',
+                      value          : value + (infix ? `-${infix}` : ''),
+                      valueFormatted : valueFormatted
+                    })
+                  }
+                }
+              } else {
                 // Does the data tree have sizes?
                 if (isArray(data.sizes)) {
                   for (const size of data.sizes) {
                     const skipSize = (data.skipSizes || []).indexOf(size) !== -1
-
-                    let valueFormatted = valueFormat
-                      .replace(new RegExp('%%prefix%%', 'g'), prefix)
-                      .replace(new RegExp('%%infix%%', 'g'), infix ? `-${infix}` : '')
+                    let valueFormatted = valueFormat.replace(new RegExp('%%prefix%%', 'g'), prefix)
 
                     if (!skipSize) {
                       valueFormatted = valueFormatted.replace(new RegExp('%%size%%', 'g'), size)
@@ -121,132 +171,98 @@ try {
                       flat           : data.flat || false,
                       prefixValue    : prefixValue,
                       template       : data.template || '',
-                      title          : parseTitle(data.title, prefix.toString(), { bp, emptyTitle, size }),
+                      title          : parseTitle(data.title, prefix.toString(), { emptyTitle, size }),
                       type           : 'tag',
-                      value          : value + (infix ? `-${infix}` : '') + (!skipSize ? `-${size}` : ''),
+                      value          : value + (!skipSize ? `-${size}` : ''),
                       valueFormatted : valueFormatted
                     })
                   }
 
                   // Nup, no sizing here
                 } else {
-                  const valueFormatted = valueFormat
-                    .replace(new RegExp('%%prefix%%', 'g'), prefix)
-                    .replace(new RegExp('%%infix%%', 'g'), infix ? `-${infix}` : '')
-
                   categories[category][subcategory].content.push({
                     prefixToFolders,
                     flat           : data.flat || false,
                     prefixValue    : prefixValue,
                     template       : data.template || '',
-                    title          : parseTitle(data.title, prefix.toString(), { bp, emptyTitle }),
+                    title          : prefixIsAdvanced ? prefixTitle : parseTitle(data.title, prefix.toString(), { emptyTitle }),
                     type           : 'tag',
-                    value          : value + (infix ? `-${infix}` : ''),
-                    valueFormatted : valueFormatted
+                    value          : value,
+                    valueFormatted : valueFormat.replace(new RegExp('%%prefix%%', 'g'), prefix)
                   })
                 }
-              }
-            } else {
-              // Does the data tree have sizes?
-              if (isArray(data.sizes)) {
-                for (const size of data.sizes) {
-                  const skipSize = (data.skipSizes || []).indexOf(size) !== -1
-                  let valueFormatted = valueFormat.replace(new RegExp('%%prefix%%', 'g'), prefix)
-
-                  if (!skipSize) {
-                    valueFormatted = valueFormatted.replace(new RegExp('%%size%%', 'g'), size)
-                  }
-
-                  categories[category][subcategory].content.push({
-                    prefixToFolders,
-                    flat           : data.flat || false,
-                    prefixValue    : prefixValue,
-                    template       : data.template || '',
-                    title          : parseTitle(data.title, prefix.toString(), { emptyTitle, size }),
-                    type           : 'tag',
-                    value          : value + (!skipSize ? `-${size}` : ''),
-                    valueFormatted : valueFormatted
-                  })
-                }
-
-                // Nup, no sizing here
-              } else {
-                categories[category][subcategory].content.push({
-                  prefixToFolders,
-                  flat           : data.flat || false,
-                  prefixValue    : prefixValue,
-                  template       : data.template || '',
-                  title          : prefixIsAdvanced ? prefixTitle : parseTitle(data.title, prefix.toString(), { emptyTitle }),
-                  type           : 'tag',
-                  value          : value,
-                  valueFormatted : valueFormat.replace(new RegExp('%%prefix%%', 'g'), prefix)
-                })
               }
             }
           }
         }
       }
-    }
 
-    let groupedItems = {}
-    let contentTemplate
-    let tagTemplate
-    let tagParentTemplate
+      let groupedItems = {}
+      let contentTemplate
+      let tagTemplate
+      let tagParentTemplate
 
-    Object.keys(categories).forEach(category => {
-      let categoryTemplate
-      let categoryTemplateDefault
+      Object.keys(categories).forEach(category => {
+        let categoryTemplate
+        let categoryTemplateDefault
 
-      // Load the template for the current category
-      if (category.startsWith(pathPrefixTags)) {
-        categoryTemplate = tagTemplate || loadTemplateForCategory('tag')
-        categoryTemplateDefault = tagParentTemplate || loadTemplateForCategory('tag-parent')
-      } else if (category.startsWith(pathPrefixApps)) {
-        categoryTemplate = contentTemplate || loadTemplateForCategory('content')
-        categoryTemplateDefault = contentTemplate || loadTemplateForCategory('content-parent')
-      } else {
-        return
-      }
-
-      if (!groupedItems[category]) {
-        groupedItems[category] = {
-          category,
-          categoryTemplate,
-          categoryTemplateDefault,
-          items: [],
+        // Load the template for the current category
+        if (category.startsWith(pathPrefixTags)) {
+          categoryTemplate = tagTemplate || loadTemplateForCategory('tag')
+          categoryTemplateDefault = tagParentTemplate || loadTemplateForCategory('tag-parent')
+        } else if (category.startsWith(pathPrefixApps)) {
+          categoryTemplate = contentTemplate || loadTemplateForCategory('content')
+          categoryTemplateDefault = contentTemplate || loadTemplateForCategory('content-parent')
+        } else {
+          return
         }
-      }
 
-      Object.keys(categories[category]).forEach(subcategory => {
-        const subcategoryContent = categories[category][subcategory].content
+        if (!groupedItems[category]) {
+          groupedItems[category] = {
+            category,
+            categoryTemplate,
+            categoryTemplateDefault,
+            items: [],
+          }
+        }
 
-        groupedItems[category].items = [
-          ...groupedItems[category].items,
-          ...subcategoryContent.map(content => ({
-            subcategory,
-            content
-          }))
-        ]
+        Object.keys(categories[category]).forEach(subcategory => {
+          const subcategoryContent = categories[category][subcategory].content
+
+          groupedItems[category].items = [
+            ...groupedItems[category].items,
+            ...subcategoryContent.map(content => ({
+              subcategory,
+              content
+            }))
+          ]
+        })
       })
-    })
 
-    let totalNumberOfTags = 0
-    each(groupedItems, category => {
-      category.items.forEach(item => {
-        generateContent(
-          category.categoryTemplate,
-          category.categoryTemplateDefault,
-          category.category,
-          item.subcategory,
-          item.content
-        )
+      let totalNumberOfTags = 0
+      each(groupedItems, category => {
+        category.items.forEach(item => {
+          generateContent(
+            category.categoryTemplate,
+            category.categoryTemplateDefault,
+            category.category,
+            item.subcategory,
+            item.content
+          )
 
-        totalNumberOfTags++
+          totalNumberOfTags++
+        })
       })
-    })
 
-    console.log(colors.blue('Total number of files generated:'), totalNumberOfTags)
-  })
+      console.log(colors.blue('Total number of files generated:'), totalNumberOfTags)
+    })
+  };
+
+  const configSubDirectories = getDirectories(currentPath(configPath));
+  configSubDirectories.forEach((configSubDirectory, index) => {
+      parseFile(currentPath(`${configSubDirectory}/config.yml`));
+  });
+
 } catch (e) {
   console.log(e)
 }

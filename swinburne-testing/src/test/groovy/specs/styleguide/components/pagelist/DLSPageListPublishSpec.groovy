87be7d4a9c6ecd1173ff654c3ab1d2ccf7276f9d
Stewart Leach <stewart.leach@isobar.com>
2019-03-05 12:31:41 +1100

    publish tests all passing

--- swinburne-testing/src/test/groovy/specs/styleguide/components/pagelist/DLSPageListPublishSpec.groovy
+++ swinburne-testing/src/test/groovy/specs/styleguide/components/pagelist/DLSPageListPublishSpec.groovy
@@ -22,7 +22,7 @@ class DLSPageListPublishSpec extends ComponentSpec {
         given: '>the page hierarchy is created as "Components" > "Lists" > "Page List"'
         and: '>I am in the component showcase page'
         and: '>the component is on the showcase page'
-        def selector = "reference_OXHFFXJJE #pagelist36"
+        def selector = "#reference_OXHFFXJJE #pagelist36"
 
         when: "I am on the component showcase page"
         setWindowSize(viewport)
@@ -33,14 +33,102 @@ class DLSPageListPublishSpec extends ComponentSpec {
         takeScreenshot($(selector).firstElement(), "The component should be on the page")
 
         and: "Has 4 list items"
-        //todo: this will be refactored when we change from using content-reference. It currently pulls in many as the references are to the same pagelist
         assert $("${selector} > div > ul > li").size() == 4
 
         and: "First card has yellow background colour when default background"
-        assert $("${selector} > div > ul > li.first > div > div.card-body").css("background-color") == "#ffeb14"
+        assert $("${selector} > div > ul > li.first > div > div.card-body").css("background-color") == "rgba(255, 235, 20, 1)" //#ffeb14
+
+        and: "last card has white background colour when default background"
+        assert $("${selector} > div > ul > li.last > div > div.card-body").css("background-color") == "rgba(255, 255, 255, 1)" //#f
 
         where:
         viewport << getViewPorts()
     }
 
+    @Unroll("Page List: Card with Image, Tag, Title and Action - Promoted (default background with charcoal theme) in #viewport.label")
+    def "Page List: Card with Image, Tag, Title and Action - Promoted (default background with charcoal theme)"() {
+
+        given: '>the page hierarchy is created as "Components" > "Lists" > "Page List"'
+        and: '>I am in the component showcase page'
+        and: '>the component is on the showcase page'
+        def selector = "#reference_1XHFFXJJE #pagelistxxx"
+
+        when: "I am on the component showcase page"
+        setWindowSize(viewport)
+        waitForAuthorPreviewPage()
+
+        then: "The component should be on the page"
+        def component = waitForComponent(selector)
+        takeScreenshot($(selector).firstElement(), "The component should be on the page")
+
+        and: "Has 4 list items"
+        assert $("${selector} > div > ul > li").size() == 4
+
+        and: "First card has yellow background colour when default background"
+        assert $("${selector} > div > ul > li.first > div > div.card-body").css("background-color") == "rgba(255, 235, 20, 1)" //#ffeb14
+
+        and: "Last card has charcoal background colour when default background"
+        assert $("${selector} > div > ul > li.last > div > div.card-body").css("background-color") == "rgba(36, 35, 35, 1)" //#242323
+
+        where:
+        viewport << getViewPorts()
+    }
+
+    @Unroll("Page List: Card with Image, Tag, Title and Action - Promoted (grey background) in #viewport.label")
+    def "Page List: Card with Image, Tag, Title and Action - Promoted (grey background)"() {
+
+        given: '>the page hierarchy is created as "Components" > "Lists" > "Page List"'
+        and: '>I am in the component showcase page'
+        and: '>the component is on the showcase page'
+        def selector = "#reference_2XHFFXJJE #pagelist36"
+
+        when: "I am on the component showcase page"
+        setWindowSize(viewport)
+        waitForAuthorPreviewPage()
+
+        then: "The component should be on the page"
+        def component = waitForComponent(selector)
+        takeScreenshot($(selector).firstElement(), "The component should be on the page")
+
+        and: "Has 4 list items"
+        assert $("${selector} > div > ul > li").size() == 4
+
+        and: "First card has charcoal background colour when default background"
+        assert $("${selector} > div > ul > li.first > div > div.card-body").css("background-color") == "rgba(36, 35, 35, 1)" //#242323
+
+        and: "Last card has white background colour when default background"
+        assert $("${selector} > div > ul > li.last > div > div.card-body").css("background-color") == "rgba(255, 255, 255, 1)" //#f
+
+        where:
+        viewport << getViewPorts()
+    }
+
+    @Unroll("Page List: Card with Image, Tag, Title and Action - Promoted (yellow background) in #viewport.label")
+    def "Page List: Card with Image, Tag, Title and Action - Promoted (yellow background)"() {
+
+        given: '>the page hierarchy is created as "Components" > "Lists" > "Page List"'
+        and: '>I am in the component showcase page'
+        and: '>the component is on the showcase page'
+        def selector = "#reference_4XHFFXJJE #pagelist36"
+
+        when: "I am on the component showcase page"
+        setWindowSize(viewport)
+        waitForAuthorPreviewPage()
+
+        then: "The component should be on the page"
+        def component = waitForComponent(selector)
+        takeScreenshot($(selector).firstElement(), "The component should be on the page")
+
+        and: "Has 4 list items"
+        assert $("${selector} > div > ul > li").size() == 4
+
+        and: "First card has charcoal background colour when default background"
+        assert $("${selector} > div > ul > li.first > div > div.card-body").css("background-color") == "rgba(36, 35, 35, 1)" //#242323
+
+        and: "Last card has white background colour when default background"
+        assert $("${selector} > div > ul > li.last > div > div.card-body").css("background-color") == "rgba(255, 255, 255, 1)" //#fff
+
+        where:
+        viewport << getViewPorts()
+    }
 }

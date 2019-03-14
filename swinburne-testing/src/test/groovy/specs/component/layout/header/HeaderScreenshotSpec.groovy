30d37b081f144078cd8b37bf6a14ad2366ec007e
stephane_swinburne <stephane.lemaitre@isobar.com>
2019-03-14 16:47:34 +1100

    [SWU-85] Adding screenshot test.

--- swinburne-testing/src/test/groovy/specs/component/layout/header/HeaderScreenshotSpec.groovy
+++ swinburne-testing/src/test/groovy/specs/component/layout/header/HeaderScreenshotSpec.groovy
@@ -7,21 +7,21 @@ import support.ComponentSpec
 @Stepwise
 class HeaderScreenshotSpec extends ComponentSpec {
 
-    String pathPage = "component/layout/header"
+    String pathPage = "styleguide/components/global-header"
     String pathSite = "content/swinburne-showcase"
     String language = "en"
-    String componentPath = "jcr:content/article/par/contentblock1/par/header"
+    String componentPath = "jcr:content/article/par/contentblock/par/reference"
 
     def setupSpec() {
         loginAsAdmin()
     }
 
-    @Unroll("Appearance of Component in #viewport.label")
+    @Unroll("Appearance of Header")
     def "Appearance of Component"() {
 
         given: '>I am in the component showcase page'
         and: '>the component is on the showcase page'
-        def selector = "#plainheader"
+        def selector = "#header_FIEH48SBE"
 
         when: 'I am in the component showcase page'
         setWindowSize(viewport)

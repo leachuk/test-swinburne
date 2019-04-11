51f2a46433399869f93d66eccd9dadc1cff570c7
Chris Shaw <chris.shaw@isobar.com>
2019-04-11 11:10:31 +1000

    SWU-736 - Finished the desktop implementation
    - Migrated some logo styles to a Sass Map
    - Moved some static values to variables
    - Undid some Prettier changes
    - Added `logo-size` function to retrieve the width/height of logos
    - Implemented the submit action on the search form
    - Added a styleguide specific fix on extra large screens

--- swinburne-aem-prototype/source/sut/js/modules/header/quick-search.ts
+++ swinburne-aem-prototype/source/sut/js/modules/header/quick-search.ts
@@ -3,14 +3,32 @@ import { sanitizeHTML } from '@global/utilities/dom'
 // Internal
 const QUICK_SEARCH_CLASS = '.brand-header__quick-search'
 
+let searchField: HTMLInputElement
+let searchForm: HTMLFormElement
+let submitButton: HTMLElement
+
+/**
+ * Submits the user input to the defined action URL set in the backend.
+ *
+ * @return void
+ */
+function submitSearchForm() {
+  const userInput = encodeURIComponent(sanitizeHTML(searchField.value))
+
+  if (userInput.length) {
+    window.location.href = searchForm.action.replace('{{query}}', userInput)
+  }
+}
+
 export default () => {
   const quickSearch = document.querySelector(QUICK_SEARCH_CLASS)
 
   if (quickSearch) {
-    const searchField: HTMLInputElement = quickSearch.querySelector('input[type=search]') as HTMLInputElement
-    const submitButton: HTMLElement = quickSearch.querySelector('button[type=submit]') as HTMLElement
+    searchField  = quickSearch.querySelector('input[type=search]') as HTMLInputElement
+    searchForm   = quickSearch.querySelector('form') as HTMLFormElement
+    submitButton = quickSearch.querySelector('button[type=submit]') as HTMLElement
 
-    if (submitButton && searchField) {
+    if (searchField && searchForm && submitButton) {
       submitButton.addEventListener('click', (event: Event) => {
         event.preventDefault()
 
@@ -21,12 +39,13 @@ export default () => {
           // miss this action.
           setTimeout(() => searchField.focus(), 1)
         } else {
-          console.info(
-            'Normal submission!\n',
-            'Field Value:', sanitizeHTML(searchField.value), '\n',
-            'Field Value (URI encoded):', encodeURIComponent(sanitizeHTML(searchField.value)),
-          )
+          submitSearchForm()
         }
+      })
+
+      searchForm.addEventListener('submit', (event: Event) => {
+        event.preventDefault()
+        submitSearchForm()
       })
     }
 

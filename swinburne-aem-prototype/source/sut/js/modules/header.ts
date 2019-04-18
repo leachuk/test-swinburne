202d010052d1022471b9b093b138c54d6a50a47f
Chris Shaw <chris.shaw@isobar.com>
2019-04-18 14:28:44 +1000

    SWU-634 - Fixed some broken icons and spacing
    - Moved the header icons JS into the global file
    - Fixed the icon spacing and alignment on all buttons
    - Fixed a card style that was incorrectly applying to child elements of body children
    - Removed the left padding from the styleguide navigation links

--- swinburne-aem-prototype/source/sut/js/modules/header.ts
+++ swinburne-aem-prototype/source/sut/js/modules/header.ts
@@ -1,18 +1,10 @@
 import NavToggler from '@global/modules/header/nav-toggler'
 import QuickSearch from '@global/modules/header/quick-search'
 
-import { isAuthorEditMode } from '@global/utilities/aem'
-
 export default async () => {
   // Header navigation toggler
   NavToggler()
 
   // Quick search toggle
   QuickSearch()
-
-  // Append Font Awesome icons to the navigation links
-  if (!isAuthorEditMode()) {
-    const { default: Icons } = await import(/* webpackChunkName: "header-icons" */ '@global/modules/header/icons')
-    Icons()
-  }
 }
--- swinburne-aem-prototype/source/sut/js/modules/icons.ts
+++ swinburne-aem-prototype/source/sut/js/modules/icons.ts
@@ -1,17 +1,15 @@
+const longArrowRight = document.createElement('i')
+longArrowRight.setAttribute('class', 'fal fa-long-arrow-right')
+
 export default () => {
-  // Links component
-  //
-  // This applies only to any link components that have the classes: `.link` & `.btn`. We
-  // don't need to apply an icon on any other form of a 'button' because they don't fall
-  // under this fallback.
-  const links = document.querySelectorAll('.link.btn')
+
+  // Links
+  const links = document.querySelectorAll('.link.btn, .btn.btn-link, header .nav-link')
 
   if (links.length) {
-    const icon = document.createElement('i')
-    icon.setAttribute('class', 'fal fa-long-arrow-right')
-
     for (const link of links) {
-      link.appendChild(icon.cloneNode())
+      link.appendChild(longArrowRight.cloneNode())
     }
   }
+
 }

5dd1bddf19dd30146a37de77bf61aab2653d9360
Chris Shaw <chris.shaw@isobar.com>
2019-04-23 14:48:40 +1000

    SWU-634 - Finished author fixes
    - Simplified the author checks for elements
    - Moved the author code into a dynamically loaded file

--- swinburne-aem-prototype/source/sut/js/modules/icons.ts
+++ swinburne-aem-prototype/source/sut/js/modules/icons.ts
@@ -1,16 +1,6 @@
 import { isAuthorEditMode } from '@utility/aem'
-import { hasParent, matches } from '@utility/dom'
-
-interface ComponentConfig {
-  [key: string]: {
-    icon: Element,
-    selectors: string[],
-  }
-}
 
 // Internal
-const bannedNodes = ['#comment', 'script', 'style']
-
 const longArrowRight = document.createElement('i')
 longArrowRight.setAttribute('class', 'icon fal fa-long-arrow-right')
 
@@ -21,8 +11,7 @@ const components: ComponentConfig = {
   },
 }
 
-export default () => {
-
+export default async () => {
   console.info('[Icons] Booting up...')
 
   // Apply icons to all components on the page by default
@@ -42,73 +31,10 @@ export default () => {
   // When in author mode, we need to watch for DOM changes so we can correctly inject the
   // icons needed dynamically.
   if (isAuthorEditMode()) {
-    console.info('[Icons] Spinning up mutation observer for AEM author mode!')
+    const {
+      default: authorIcons,
+    } = await import(/* webpackChunkName: "icons-author" */ '@module/icons-author')
 
-    const mutationObserver = new MutationObserver((mutations: MutationRecord[]) => {
-      console.info('\n[Icons] Change detected! %d mutations in play', mutations.length)
-
-      mutations.forEach(({ addedNodes }: MutationRecord) => {
-        if (!addedNodes.length) {
-          return
-        }
-
-        for (const node of addedNodes) {
-          if (
-            bannedNodes.indexOf(node.nodeName.toLowerCase()) !== -1 ||
-            (node as Element).querySelector('.icon')
-          ) {
-            continue
-          }
-
-          // Now, check through the pre-defined components
-          for (const component of Object.keys(components)) {
-            console.info('\n[Icons] Running through mutation %O against component %O', node, component)
-
-            const config = components[component]
-
-            for (const selector of config.selectors) {
-              const selectorLevels = selector.split(' ')
-              const selectorMatch  = selectorLevels.pop()
-
-              console.info('[Icons] Checking selector %s with levels:', selectorMatch, selectorLevels)
-
-              // Finally, test to ensure the node matches
-              if (selectorMatch && matches(node as Element, selectorMatch)) {
-                // If there are additional levels, use them as a way of detecting if the node exists in
-                // the correct parent tree.
-                if (selectorLevels.length) {
-                  let count = 0
-
-                  // Check the levels in reverse, this is because we want to go from the node up
-                  for (const level of selectorLevels.reverse()) {
-                    count = hasParent(node.parentNode, level) ? count + 1 : count
-                  }
-
-                  // If one or more of the parents didn't match, continue onto the next selector
-                  if (count !== selectorLevels.length - 1) {
-                    console.info('[Icons] One or more parent selectors for %s failed to match!', selectorMatch)
-                    continue
-                  }
-
-                  console.info('[Icons] Selector %s and parents matched, appending icon:', selectorMatch, config.icon)
-                } else {
-                  console.info('[Icons] Selector %s matched, appending icon:', selectorMatch, config.icon)
-                }
-
-                node.appendChild(config.icon.cloneNode())
-              } else {
-                console.info('[Icons] Selector %s invalid, continuing on...', selectorMatch)
-              }
-            }
-          }
-        }
-      })
-    })
-
-    mutationObserver.observe(document.documentElement, {
-      childList : true,
-      subtree   : true,
-    })
+    authorIcons(components)
   }
-
 }
